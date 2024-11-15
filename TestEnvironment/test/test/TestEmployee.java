3676da829440aee2095cbcbc4abfd527a6a9eed6
Ulugbek Yarkinov <72514031+UlugbekYarkinov@users.noreply.github.com>
2024-11-15 14:11:41 +0100

    first four unit test implemented

--- TestEnvironment/test/test/TestEmployee.java
+++ TestEnvironment/test/test/TestEmployee.java
@@ -27,4 +27,43 @@ class TestEmployee {
 
 	}
 
+	@Test
+	void test50Bonus() {
+		int yearOfHiring = 2021;
+		int salary = 1000;
+		
+		int yearOfCalculation = 2024;
+		
+		Employee employee = new Employee(yearOfHiring, salary);
+		
+		assertEquals(salary * 0.50, employee.bonus(yearOfCalculation));
+
+	}
+	
+	@Test
+	void test75Bonus() {
+		int yearOfHiring = 2018;
+		int salary = 1000;
+		
+		int yearOfCalculation = 2024;
+		
+		Employee employee = new Employee(yearOfHiring, salary);
+		
+		assertEquals(salary * 0.75, employee.bonus(yearOfCalculation));
+
+	}
+	
+	@Test
+	void test100Bonus() {
+		int yearOfHiring = 2010;
+		int salary = 1000;
+		
+		int yearOfCalculation = 2024;
+		
+		Employee employee = new Employee(yearOfHiring, salary);
+		
+		assertEquals(salary, employee.bonus(yearOfCalculation));
+
+	}
+
 }
