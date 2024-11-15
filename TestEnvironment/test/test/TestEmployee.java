dda68c0c7c84586ba928ffa73b3eda910c967f7c
Ulugbek Yarkinov <72514031+UlugbekYarkinov@users.noreply.github.com>
2024-11-15 14:30:11 +0100

    the rest test cases implemented

--- TestEnvironment/test/test/TestEmployee.java
+++ TestEnvironment/test/test/TestEmployee.java
@@ -13,7 +13,8 @@ class TestEmployee {
 	public static void info() {
 		System.out.println("Employee version: " + Employee.version());
 	}
-	
+
+	//Test Case 1
 	@Test
 	void testNoBonus() {
 		int yearOfHiring = 2022;
@@ -27,6 +28,7 @@ class TestEmployee {
 
 	}
 
+	//Test Case 2
 	@Test
 	void test50Bonus() {
 		int yearOfHiring = 2021;
@@ -39,7 +41,8 @@ class TestEmployee {
 		assertEquals(salary * 0.50, employee.bonus(yearOfCalculation));
 
 	}
-	
+
+	//Test Case 3
 	@Test
 	void test75Bonus() {
 		int yearOfHiring = 2018;
@@ -52,7 +55,8 @@ class TestEmployee {
 		assertEquals(salary * 0.75, employee.bonus(yearOfCalculation));
 
 	}
-	
+
+	//Test Case 4
 	@Test
 	void test100Bonus() {
 		int yearOfHiring = 2010;
@@ -63,6 +67,63 @@ class TestEmployee {
 		Employee employee = new Employee(yearOfHiring, salary);
 		
 		assertEquals(salary, employee.bonus(yearOfCalculation));
+
+	}
+
+	//Test Case 5
+	@Test
+	void testInvalidSalary() {
+		int yearOfHiring = 2010;
+		int salary = -1000;
+		
+		int yearOfCalculation = 2024;
+		
+		Employee employee = new Employee(yearOfHiring, salary);
+		
+		assertThrows(RuntimeException.class, () -> employee.bonus(yearOfCalculation));
+
+	}
+	
+	//Test Case 6
+	@Test
+	void testInvalidYearOfHiring() {
+		int yearOfHiring = 1989;
+		int salary = 1000;
+		
+		int yearOfCalculation = 2024;
+		
+		Employee employee = new Employee(yearOfHiring, salary);
+		
+		assertThrows(RuntimeException.class, () -> employee.bonus(yearOfCalculation));
+
+	}
+	
+	//Test Case 7
+	@Test
+	void testInvalidYearOfCalculation() {
+		int yearOfHiring = 2024;
+		int salary = 1000;
+		
+		int yearOfCalculation = 2021;
+		
+		Employee employee = new Employee(yearOfHiring, salary);
+		
+		assertThrows(Exception.class, () -> employee.bonus(yearOfCalculation));
+
+	}
+	
+	//Test Case 8
+	@Test
+	void testNullInputs() {
+		Integer yearOfHiring = null;
+		
+		Integer salary = null;
+		
+		Integer yearOfCalculation = null;
+		
+		Employee employee = new Employee(yearOfHiring, salary);
+		
+		assertThrows(NullPointerException.class, () -> employee.bonus(yearOfCalculation));
 
 	}
 
